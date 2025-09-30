01ae8739e7a9c497b5c2b7987678802022bb7951
ashenafi-16 <ashumulugeta016@gmail.com>
2025-09-30 14:13:16 +0300

    add watch/services.py

--- movie_recommendation_web/settings.py
+++ movie_recommendation_web/settings.py
@@ -168,7 +168,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://your-frontend.example",
     "http://localhost:3000",
 ]
-ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 CORS_ALLOW_CREDENTIALS = True   # MUST for cookies across origins
 # REST_FRAMEWORK = {
 #     'DEFAULT_AUTHENTICATIOIN_CLASSES': (
